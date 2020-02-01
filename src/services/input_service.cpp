8f4823db993e0237094bb0e37d65aa183b982bcc
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-02-01 15:28:14 +0000

    WIP input

--- src/services/input_service.cpp
+++ src/services/input_service.cpp
@@ -1,1 +1,37 @@
 #include "input_service.h"
+
+InputService::InputService(InputAPI& input)
+{
+	device = std::make_shared<InputVirtual>(12, 2);
+
+	auto key = input.getKeyboard();
+	if (key) {
+		device->bindButton(0, key, Keys::S);
+		device->bindButton(1, key, Keys::D);
+		device->bindButton(2, key, Keys::A);
+		device->bindButton(3, key, Keys::W);
+
+		device->bindButton(0, key, Keys::Down);
+		device->bindButton(1, key, Keys::Right);
+		device->bindButton(2, key, Keys::Left);
+		device->bindButton(3, key, Keys::Up);
+	}
+	
+	auto joy = input.getJoystick();
+	if (joy) {
+		device->bindButton(0, joy, joy->getButtonAtPosition(JoystickButtonPosition::FaceBottom));
+		device->bindButton(1, joy, joy->getButtonAtPosition(JoystickButtonPosition::FaceRight));
+		device->bindButton(2, joy, joy->getButtonAtPosition(JoystickButtonPosition::FaceLeft));
+		device->bindButton(3, joy, joy->getButtonAtPosition(JoystickButtonPosition::FaceTop));
+	}
+}
+
+void InputService::update(Time t)
+{
+	device->update(t);
+}
+
+InputVirtual& InputService::getInput()
+{
+	return *device;
+}
--- src/services/input_service.h
+++ src/services/input_service.h
@@ -4,5 +4,13 @@
 using namespace Halley;
 
 class InputService : public Service {
+public:
+	InputService(InputAPI& api);
 
+	void update(Time t);
+
+	InputVirtual& getInput();
+
+private:
+	std::shared_ptr<InputVirtual> device;
 };
