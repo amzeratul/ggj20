2e6f581dd59fdc0045a4cbf4affff93b100ca2d5
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-03-08 13:14:11 +0000

    Update Halley

---
+++ scripts/cmake_vs2019_64_with_tools.bat
@@ -0,0 +1,17 @@
+set PreferredToolArchitecture=x64
+cd ..
+mkdir build
+cd build
+cmake -G "Visual Studio 16 2019" ^
+    -A x64 ^
+    -DHALLEY_PATH=../halley ^
+    -DCMAKE_INCLUDE_PATH="lib\include;C:\dev\include;R:\dev\include" ^
+    -DCMAKE_LIBRARY_PATH="lib\windows64;C:\dev\lib64;R:\dev\lib64" ^
+    -DBOOST_LIBRARYDIR="c:\Boost\lib" ^
+    -DBOOST_INCLUDEDIR="c:\Boost\include\boost-1_66" ^
+    -DBoost_USE_STATIC_LIBS=1 ^
+    -DBUILD_HALLEY_TOOLS=1 ^
+    -DBUILD_HALLEY_TESTS=0 ^
+    -DUSE_PCH=1 ^
+    ..
+pause
